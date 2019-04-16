f3fb9a886676c6256a25fd6a6befe31492aaeb1d
moda <819905173@qq.com>
2019-04-16 15:23:46 +0800

    (*) 继承feign

---
+++ service-feign/src/main/java/com/moda/cloud/servicefeign/service/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.moda.cloud.servicefeign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author YangCH
+ * create on 2019/4/16
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
